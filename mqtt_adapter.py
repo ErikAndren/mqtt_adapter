3af41ce219b26ccffc0265cc0a0271ee3f1ba120
Erik Andrén Zachrisson <erik@zachrisson.info>
2018-09-28 21:13:19 +0200

    Remove timing information and forward message

--- mqtt_adapter.py
+++ mqtt_adapter.py
@@ -1,22 +1,24 @@
-
+#!/usr/bin/env python
 
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
+import json
 
 topic = "tele/sonoff/RESULT/#"
 
-
-
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code " + str(rc))
-
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe(topic)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    msg_json = json.loads(str(msg.payload))
+
+    # Verify message to at least some extent
+    if msg_json['RfRaw']['Data'][0:4] == "AAA4" and msg_json['RfRaw']['Data'][22:24] == "55":
+        publish.single("tele/sonoff/door_sensor", msg_json['RfRaw']['Data'][16:22], hostname = "phobos")
 
 client = mqtt.Client()
 client.on_connect = on_connect
